7fbe972b612bfc383fb7cca0ab054f58410a7842
00Kai0 <sunkaihuisos@gmail.com>
2020-08-23 18:44:02 +0800

    support command lines

--- README.md
+++ README.md
@@ -5,6 +5,17 @@ Maybe it is not good enough right now, I hope you could find something fun in th
 ## Build And Run
 `git clone` this repo and run `make` to build it.
 
+Show Options:
+```bash
+kai@kai:~/myBrainFk$ ./bin/bf --help
+Usage: bf [option] | file
+A Sample BrainFuck interpreter.
+
+-h --help      show usage message .
+-e --exec      execute BrainFuck code .
+   --version   output version information and exit .
+```
+
 Run example:
 ```bash
 kai@kai:~/myBrainFk$ ./bin/bf example/hello.bf
@@ -13,7 +24,7 @@ kai@kai:~/myBrainFk$ cat example/hello.bf
 ++++++++++[>+++++++>++++++++++>+++>+<<<<-]
 >++.>+.+++++++..+++.>++.<<+++++++++++++++.
 >.+++.------.--------.>+.>.
-kai@kai:~/work/c_lab/myBrainFk$ 
+kai@kai:~/myBrainFk$
 ```
 
 ## Other Funny code
--- bf.c
+++ bf.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
+#ifndef VERSION
+# define VERSION
+# define MAIN_VERSION 0
+# define SUB_VERSION 0
+# define LAST_VERSION 1
+#endif
 
 typedef enum node_type {
     HEAD=1,
@@ -36,6 +44,7 @@ new_node(node* nodep, node_type type)
     nodep = nodep->next;
     nodep->next = NULL;
     nodep->type = type;
+    return nodep;
 }
 
 # define NEXT_NODE(ps) ps->c_node = ps->c_node->next;
@@ -48,8 +57,8 @@ init_proc_state(proc_state* p)
     p->c_node->type = HEAD;
     p->c_node->next = NULL;
     p->head_node = p->c_node;
-    p->stack = (node**)malloc(sizeof(node*)*200);
-    p->data = (char*)malloc(sizeof(char)*200);
+    p->stack = (node**)malloc(sizeof(node*)*DEFAULT_SIZE);
+    p->data = (char*)malloc(sizeof(char)*DEFAULT_SIZE);
     p->dsize = DEFAULT_SIZE;
     p->ssize = DEFAULT_SIZE;
     p->pointer = 0;
@@ -194,25 +203,8 @@ EXIT:
 
 }
 
-
 int
-main(int argc, const char** argv)
-{
-    if (argc < 2) {
-        fprintf(stderr, "Miss File.\n");
-        return 1;
-    }
-    else if (argc > 2) {
-        fprintf(stderr, "Unexceped arguments.\n");
-        return 1;
-    }
-
-    const char* filename = argv[1];
-    // printf("Loading File: %s...\n", filename);
-
-    proc_state global_proc;
-    init_proc_state(&global_proc);
-
+parse_from_file(proc_state* proc, const char* filename) {
     FILE* fp;
     fp = fopen(filename, "r");
 
@@ -222,7 +214,7 @@ main(int argc, const char** argv)
     }
 
     // parse code
-    node* nodep = global_proc.c_node;
+    node* nodep = proc->c_node;
     for (;;) {
         char c = fgetc(fp);
         if (feof(fp)) {
@@ -243,16 +235,96 @@ main(int argc, const char** argv)
     }
 
     fclose(fp);
+    return 0;
+}
 
-    // Debug print
-    //print_nodes(&global_proc);
+int
+parse_from_str(proc_state* proc, const char* string) {
+    size_t length = strlen(string);
 
+    // parse code
+    node* nodep = proc->c_node;
+    for (int i=0;i<length;i++) {
+        char c = string[i];
+
+        switch (c) {
+            case '>': nodep = new_node(nodep, POINTER_ADD);break;
+            case '<': nodep = new_node(nodep, POINTER_SUB);break;
+            case '+': nodep = new_node(nodep, BYTE_ADD);break;
+            case '-': nodep = new_node(nodep, BYTE_SUB);break;
+            case '.': nodep = new_node(nodep, OUTPUT);break;
+            case ',': nodep = new_node(nodep, INPUT);break;
+            case '[': nodep = new_node(nodep, START_LOOP);break;
+            case ']': nodep = new_node(nodep, END_LOOP);break;
+            default: continue;
+        }
+    }
+
+    return 0;
+}
+
+void
+usage()
+{
+    fprintf(stderr, "Usage: bf [option] | file\n");
+    fprintf(stderr, "A Sample BrainFuck interpreter.\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "-h --help      show usage message .\n");
+    fprintf(stderr, "-e --exec      execute BrainFuck code .\n");
+    fprintf(stderr, "   --version   output version information and exit . \n");
+}
+
+
+int
+main(int argc, const char** argv)
+{
+    proc_state global_proc;
+    init_proc_state(&global_proc);
+
+    if (argc < 2) {
+        usage();
+        return 1;
+    }
+    else {
+        int index = 1;
+        while (index<argc) {
+            if (strlen(argv[index])>1 && argv[index][0]=='-') {
+                if (strcmp(argv[index], "-h")==0 || strcmp(argv[index], "--help")==0) {
+                    usage();
+                    goto EXIT;
+                }
+                else if (strcmp(argv[index], "-e")==0 || strcmp(argv[index], "--exec")==0) {
+                    parse_from_str(&global_proc, argv[++index]);
+                }
+                else if (strcmp(argv[index], "--version")==0) {
+                    printf("bf version %d.%d.%d.\n", MAIN_VERSION, SUB_VERSION, LAST_VERSION);
+                    goto EXIT;
+                }
+                else {
+                    fprintf(stderr, "unexcepted options\n");
+                    usage();
+                    return 1;
+                }
+            } else {
+                const char* filename = argv[1];
+                if (parse_from_file(&global_proc, filename)!=0) {
+                    goto EXIT;
+                }
+                goto RUN_MODE;
+
+            }
+            
+            index += 1;
+        }
+    }
+
+RUN_MODE:
     // Run Programme
     run_programme(&global_proc);
 
+EXIT:
     return 0;
 
 ERROR:
-    fclose(fp);
     return 1;
 }
